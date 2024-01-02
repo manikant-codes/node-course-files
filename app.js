3d2d941944da1e85911050b96504f7fec9a77965
Manikant Jha <manikantcodes@gmail.com>
2024-01-02 18:09:05 +0530

    fs

--- app.js
+++ app.js
@@ -1,32 +1,55 @@
-var http = require("http");
-var fs = require("fs");
-const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
+// var http = require("http");
+// var fs = require("fs");
+// const path = require("path");
+// require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 
-const server = http.createServer(function (req, res) {
-  if (req.url === "/") {
-    console.log("process", process.env.SECRET);
-    fs.readFile("./index.html", (err, data) => {
-      if (err) {
-        res.writeHead(404, { "Content-Type": "application/json" });
-        return res.end(
-          JSON.stringify({ status: 404, message: "Error reading page!" })
-        );
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  } else if (req.url === "/about") {
-    const data = fs.readFileSync("./about.html");
-    // for (let i = 0; i < 100000000; i++) {
-    //   for (let j = 0; j < 100000000; j++) {}
-    // }
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data);
-  } else {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    res.end("<h1>404</h1>");
+// const server = http.createServer(function (req, res) {
+//   if (req.url === "/") {
+//     console.log("process", process.env.SECRET);
+//     fs.readFile("./index.html", (err, data) => {
+//       if (err) {
+//         res.writeHead(404, { "Content-Type": "application/json" });
+//         return res.end(
+//           JSON.stringify({ status: 404, message: "Error reading page!" })
+//         );
+//       }
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.end(data);
+//     });
+//   } else if (req.url === "/about") {
+//     const data = fs.readFileSync("./about.html");
+//     // for (let i = 0; i < 100000000; i++) {
+//     //   for (let j = 0; j < 100000000; j++) {}
+//     // }
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     res.end(data);
+//   } else {
+//     res.writeHead(404, { "Content-Type": "text/html" });
+//     res.end("<h1>404</h1>");
+//   }
+// });
+
+// server.listen(5050);
+
+let temp;
+
+async function getData() {
+  try {
+    const data1 = await fs.readFile("./info.txt", { encoding: "utf-8" });
+    const data2 = await fs.readFile("./info2.txt", { encoding: "utf-8" });
+    const data3 = await fs.readFile("./info3.txt", { encoding: "utf-8" });
+
+    return data1 + data2 + data3;
+  } catch (err) {
+    console.log(err);
   }
-});
+}
 
-server.listen(5050);
+getData()
+  .then((data) => {
+    temp = data;
+    console.log("temp", temp);
+  })
+  .catch((err) => console.log(err));
+
+module.exports = { temp };
--- fs-module.js
+++ fs-module.js
@@ -1,9 +1,53 @@
-// let txt = "";
+const fs = require("fs/promises");
 
-// for (let i = 0; i < 10; i++) {
-//   txt += "Hello " + i + "\n";
-// }
+// const data = fs.readFileSync("./info.txt", { encoding: "utf-8" });
 
-// fs.writeFile("./info.txt", txt, (err) => {
-//   console.log(err);
+// console.log("Start");
+
+// let temp;
+
+// fs.readFile("./info.txt", { encoding: "utf-8" }, (err1, data1) => {
+//   if (err1) {
+//     console.log(err1);
+//   } else {
+//     fs.readFile("./info2.txt", { encoding: "utf-8" }, (err2, data2) => {
+//       if (err2) {
+//         console.log(err2);
+//       } else {
+//         fs.readFile("./info3.txt", { encoding: "utf-8" }, (err3, data3) => {
+//           if (err3) {
+//             console.log(err3);
+//           } else {
+//             console.log(data1 + data2 + data3);
+//           }
+//         });
+//       }
+//     });
+//   }
 // });
+
+// fs.writeFileSync("./info2.txt", "Happy New Year!\n", { flag: "a" });
+
+// fs.writeFile("./info3.txt", "Happy New Year!", { flag: "a" }, (err) => {
+//   if (err) {
+//     console.log("err", err);
+//   }
+// });
+
+// console.log("End", temp);
+
+// function getData() {
+//   fs.readFile("./info.txt", { encoding: "utf-8" })
+//     .then((data1) => {
+//       fs.readFile("./info2.txt", { encoding: "utf-8" })
+//         .then((data2) => {
+//           fs.readFile("./info3.txt", { encoding: "utf-8" })
+//             .then((data3) => console.log(data1 + data2 + data3))
+//             .catch((err) => console.log(err));
+//         })
+//         .catch((err) => console.log(err));
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
