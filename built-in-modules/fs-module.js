083d0f34031526ba0d32f672f7a642961b8e4d98
Manikant Jha <manikantcodes@gmail.com>
2024-01-10 17:24:59 +0530

    restructured directory structure

--- built-in-modules/fs-module.js
+++ built-in-modules/fs-module.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+// const fs = require("fs");
 
 // const data = fs.readFileSync("./info.txt", { encoding: "utf-8" });
 
@@ -51,3 +51,22 @@ const fs = require("fs");
 //       console.log(err);
 //     });
 // }
+
+// async function getData() {
+//   try {
+//     const data1 = await fs.readFile("./info.txt", { encoding: "utf-8" });
+//     const data2 = await fs.readFile("./info2.txt", { encoding: "utf-8" });
+//     const data3 = await fs.readFile("./info3.txt", { encoding: "utf-8" });
+
+//     return data1 + data2 + data3;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// getData()
+//   .then((data) => {
+//     temp = data;
+//     console.log("temp", temp);
+//   })
+//   .catch((err) => console.log(err));
---
+++ concepts/timers.js
@@ -0,0 +1,27 @@
+// function a() {
+//   console.log("A!");
+// }
+
+// function b() {
+//   console.log("B!");
+// }
+
+// function c() {
+//   console.log("C!");
+// }
+
+// a();
+
+// fs.open("./info.txt", "r", (err, fd) => {
+//   console.log("err", err);
+//   console.log("Second!", fd);
+//   console.log("fd", fd);
+// });
+
+// a();
+
+// setTimeout(function myTimeout() {
+//   console.log("Inside Timeout!");
+// }, 5000);
+
+// b();
