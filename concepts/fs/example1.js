2f33836f0a0549ba29ed8df92cfb27fc4f2c72c2
Manikant Jha <manikantcodes@gmail.com>
2024-01-17 18:18:02 +0530

    fs module

--- concepts/fs/example1.js
+++ concepts/fs/example1.js
@@ -1,4 +1,8 @@
-// const fs = require("fs");
+const fs = require("fs");
+// const buffer = new Buffer.alloc(1024);
+
+// console.log("buffer", buffer);
+
 // const buffer = new Buffer("Manikant asdd asd asd asd");
 
 // fs.stat("./concepts/fs/info.txt", function (err, stats) {
@@ -11,12 +15,34 @@
 //   }
 // });
 
-// fs.open("./concepts/fs/info.txt", "r+", function (err, fileDescriptor) {
+// fd: file-descriptor
+// fs.open("./concepts/fs/info.txt", "r+", function (err, fd) {
 //   if (err) {
 //     console.log("err", err);
 //   } else {
-//     console.log("fileDescriptor", fileDescriptor);
-// fs.write(fileDescriptor, buffer, function (err, data) {
+//     // console.log("fileDescriptor", process.stdin.fd);
+//     // console.log("fileDescriptor", process.stdout.fd);
+//     // console.log("fileDescriptor", process.stderr.fd);
+//     // console.log("fileDescriptor", fd);
+
+//     fs.ftruncate(fd, 9, function (err) {
+//       fs.read(fd, buffer, 0, buffer.byteLength, 0, function (err, bytes) {
+//         if (err) {
+//           console.log("err", err);
+//         } else {
+//           console.log(bytes + " bytes read");
+//           console.log(buffer.slice(0, bytes).toString());
+//         }
+//       });
+
+//       fs.close(fd, function () {
+//         console.log("File closed!");
+//       });
+//     });
+//   }
+// });
+
+// fs.write(fd, buffer, function (err, data) {
 //   if (err) {
 //     console.log("err 1", err);
 //   } else {
@@ -24,21 +50,13 @@
 //   }
 // });
 
-//     const buffer = new Buffer(2);
+// const buffer = new Buffer(2);
 
-//     fs.read(
-//       fileDescriptor,
-//       buffer,
-//       0,
-//       buffer.byteLength,
-//       0,
-//       function (err, bytes, b) {
-//         if (err) {
-//           console.log("err", err);
-//         } else {
-//           console.log("data", bytes, b.toString());
-//         }
-//       }
-//     );
-//   }
-// });
+// fs.writeFile("./concepts/fs/info.txt", "Hello World", function () {});
+
+fs.unlink("./concepts/fs/info.txt", function (err) {
+  if (err) {
+  } else {
+    console.log("File deleted successfully.");
+  }
+});
