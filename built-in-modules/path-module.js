0136097026907c3f8e296e8115272a1fdee3a677
Manikant Jha <manikantcodes@gmail.com>
2024-04-02 15:33:28 +0530

    built-in modules

---
+++ built-in-modules/path-module.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+console.log(__filename);
+console.log(__dirname);
+
+console.log(path.basename(__filename));
+console.log(path.basename(__dirname));
+
+console.log(path.extname(__filename));
+console.log(path.extname(__dirname));
+
+console.log(path.parse(__filename));
+console.log(path.format(path.parse(__filename)));
+
+console.log(path.isAbsolute(__filename));
+console.log(path.isAbsolute("./data.json"));
+
+console.log(path.join("folder1", "folder2", "index.html"));
+console.log(path.join("/folder1", "folder2", "index.html"));
+console.log(path.join("/folder1", "//folder2", "index.html"));
+
+console.log(path.join(__dirname, "../index.html"));
