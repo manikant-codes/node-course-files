c4ce0b3f917e1bbb1db3cdadad05c90e1ac6c33b
Manikant Jha <manikantcodes@gmail.com>
2024-04-03 17:01:13 +0530

    taught callbacks and path module

--- built-in-modules/path-module.js
+++ built-in-modules/path-module.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
-console.log("__filename", __filename);
-console.log("__dirname", __dirname);
+// console.log("__filename", __filename);
+// console.log("__dirname", __dirname);
 
 // console.log(path.basename(__filename));
 // console.log(path.basename(__dirname));
@@ -12,15 +12,15 @@ console.log("__dirname", __dirname);
 // console.log(path.parse(__filename));
 // console.log(path.format(path.parse(__filename)));
 
-console.log(path.isAbsolute(__filename));
-console.log(path.isAbsolute("./data.json"));
+// console.log(path.isAbsolute(__filename));
+// console.log(path.isAbsolute("../data.json"));
 
-// console.log(path.join("folder1", "folder2", "index.html"));
+// console.log("join => ", path.join("folder1", "folder2", "index.html"));
 // console.log(path.join("/folder1", "folder2", "index.html"));
 // console.log(path.join("/folder1", "//folder2", "index.html"));
-// console.log(path.join(__dirname, "../index.html"));
+// console.log("path => ", path.join(__dirname, "folder1", "../index.html"));
 
-// console.log(path.resolve("folder1", "folder2", "index.html"));
-// console.log(path.resolve("/folder1", "folder2", "index.html"));
+// console.log("resolve => ", path.resolve("folder1", "folder2", "index.html"));
+// console.log(path.resolve("folder1", "//folder2", "index.html"));
 // console.log(path.resolve("/folder1", "//folder2", "index.html"));
-// console.log(path.resolve(__dirname, "../index.html"));
+// console.log(path.resolve("/folder1", "folder2", "../index.html"));
