92e8b35028224c3165cd982909237ea084105a6b
Manikant Jha <manikantcodes@gmail.com>
2024-04-22 17:12:58 +0530

    express and http module revision

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require("express");
-const http = require("http");
-const url = require("url");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 
-app.set("view engine", "ejs");
+// app.use(express.static("./pages"));
 
-app.get("/", (req, res) => {
-  //   res.sendStatus(404);
-  //   res.send("Hello World!");
-  //   res.status(404).json({ msg: "Resource not found!" });
-  //   res.download("flower.jpg");
-  res.render("index", { msg: "Hello Express" });
+app.get("/", (req, res, next) => {
+  // res.sendStatus(404);
+  // res.send("Hello World!");
+  // res.json({ msg: "Hello World!" });
+  // res.sendFile(path.join(__dirname, "/pages/index.html"));
+  // res.status(404).send("Not Found!");
+  // res.status(404).json("Not Found!");
+  res.status(200).sendFile(path.join(__dirname, "/pages/index.html"));
 });
 
+// app.get("/index.css", (req, res, next) => {
+//   res.status(200).sendFile(path.join(__dirname, "/pages/index.css"));
+// });
+
 app.listen(5000, () => {
-  console.log("The server is listening on port 5000!");
+  console.log("Server is listening on port 5000!");
 });
