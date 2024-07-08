e139548b0576e4c509b9584e380e061be8f7b589
Manikant Jha <being.manikantjha@gmail.com>
2024-07-08 17:36:50 +0530

    changes in tasks.js

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -15,15 +15,15 @@ const getSingleTask = (req, res) => {
 
 const addTask = async (req, res) => {
   try {
-    const { task, isCompleted, priority, dueDate } = req.body;
+    const { task, priority, dueDate } = req.body;
     // const task = new Task();
     // task.task = req.body.task;
     // task.isCompleted = req.body.isCompleted;
     // task.priority = req.body.priority;
     // task.dueDate = req.body.dueDate;
     // await task.save();
-    await Task.create({ task, isCompleted, priority, dueDate });
-    res.status(200).json({ success: true, data: task });
+    const result = await Task.create({ task, priority, dueDate });
+    res.status(200).json({ success: true, data: result });
   } catch (error) {
     console.log("Error: ", error);
     res.status(500).json({ success: false, msg: "Failed to add task!" });
