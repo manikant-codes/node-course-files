dedc293a1ad73cfb5331a14724e9640f096566d5
Manikant Jha <manikantcodes@gmail.com>
2024-07-12 11:49:11 +0530

    taught file rename and file delete

--- 04-fs-module.js
+++ 04-fs-module.js
@@ -1,7 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 
-const filePath = path.join(__dirname, "folderOne", "folderTwo", "info.csv");
+const filePath = path.join(__dirname, "folderOne", "folderTwo", "info.txt");
+
+const filePathNew = path.join(
+  __dirname,
+  "folderOne",
+  "folderTwo",
+  "changed-file-name.txt"
+);
 
 console.log("Log 1");
 
@@ -15,11 +22,25 @@ console.log("Log 1");
 // const data = fs.readFileSync(filePath);
 // console.log("data", data.toString());
 
-fs.readFile(filePath, (err, data) => {
-  if (err) {
-    console.log("Error: ", err);
-  }
-  console.log("data", data.toString());
-});
+// fs.readFile(filePath, "utf8", (err, data) => {
+//   if (err) {
+//     return console.log("Error: ", err);
+//   }
+//   console.log("data", data);
+// });
 
-console.log("Log 2");
+// console.log("Log 2");
+
+// fs.unlink(filePath, (err) => {
+//   if (err) {
+//     return console.log(err);
+//   }
+//   console.log("File deleted successfully!");
+// });
+
+fs.rename(filePath, filePathNew, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("File renamed successfully!");
+});
