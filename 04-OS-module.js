e5861566faa4ba568fbc199a3fcb8c0469a3701d
Manikant Jha <manikantcodes@gmail.com>
2024-07-12 18:25:50 +0530

    taught os and fs module

--- 04-OS-module.js
+++ 04-OS-module.js
@@ -1,6 +1,12 @@
 const os = require("os");
 
-// console.log("arch", os.arch());
-// console.log("arch", os.constants);
-// console.log("arch", os.cpus());
-// console.log("arch", os.freemem() / 1024 / 1024 / 1024);
+// console.log("arch", os.arch()); // cpu architecture
+// console.log("cpus", os.cpus()); // cup info
+// console.log("totalmem", os.totalmem() / 1024 / 1024 / 1024); // total ram
+// console.log("freemem", os.freemem() / 1024 / 1024 / 1024); // free ram
+// console.log("type", os.type()); // os type
+// console.log("uptime", os.uptime() / 60 / 60); // uptime in seconds
+// console.log("tmpdir", os.tmpdir()); // path to temp directory
+// console.log("release", os.release()); // os version
+// console.log("endianness", os.endianness());
+// console.log("networkInterfaces", os.networkInterfaces());
---
+++ 05-fs-module.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname, "files", "info.csv");
+
+// fs.writeFile(filePath, "manikant,10,45,56,89", (err) => {
+//   if (err) {
+//     return console.log("Error: ", err);
+//   }
+//   console.log("Write completed!");
+// });
+
+// fs.writeFileSync(filePath, "hello world");
