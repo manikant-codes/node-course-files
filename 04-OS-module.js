9851ad6b4e9d5f927e45837c61ac0cc4d81ca0f1
Manikant Jha <manikantcodes@gmail.com>
2024-07-12 10:58:50 +0530

    os module

--- 04-OS-module.js
+++ 04-OS-module.js
@@ -3,4 +3,4 @@ const os = require("os");
 // console.log("arch", os.arch());
 // console.log("arch", os.constants);
 // console.log("arch", os.cpus());
-console.log("arch", os.freemem() / 1024 / 1024 / 1024);
+// console.log("arch", os.freemem() / 1024 / 1024 / 1024);
