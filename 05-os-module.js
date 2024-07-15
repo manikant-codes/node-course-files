f9cc994938831333916485b4306f8b97e0a1e09d
Manikant Jha <manikantcodes@gmail.com>
2024-07-15 12:14:17 +0530

    taught OS fs append and os  module

---
+++ 05-os-module.js
@@ -0,0 +1,10 @@
+const os = require("os");
+
+// console.log("arch()", os.arch());
+// console.log("constants", os.constants);
+// console.log("cpus()", os.cpus());
+// console.log("freemem()", os.freemem() / 1024 / 1024 / 1024);
+// console.log("totalmem()", os.totalmem() / 1024 / 1024 / 1024);
+// console.log("release()", os.release());
+// console.log("platform()", os.platform());
+// console.log("type()", os.type());
