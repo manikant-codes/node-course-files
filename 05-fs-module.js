fdb639bd30c42d59d8fa03bc4986f2518a3df756
Manikant Jha <manikantcodes@gmail.com>
2024-07-15 18:22:40 +0530

    taught fs module

--- 05-fs-module.js
+++ 05-fs-module.js
@@ -1,13 +1,44 @@
 const fs = require("fs");
 const path = require("path");
 
-const filePath = path.join(__dirname, "files", "info.csv");
+const filePath = path.join(__dirname, "files", "renamed.txt");
+const filePathNew = path.join(__dirname, "files", "info.txt");
 
-// fs.writeFile(filePath, "manikant,10,45,56,89", (err) => {
+// Write File
+fs.writeFile(filePath, "good evening", (err) => {
+  if (err) {
+    return console.log("Error: ", err);
+  }
+  console.log("Write completed!");
+});
+
+// fs.writeFileSync(filePath, "hello world");
+
+// Read File
+// fs.readFile(filePath, (err, data) => {
 //   if (err) {
 //     return console.log("Error: ", err);
 //   }
-//   console.log("Write completed!");
+//   console.log("data", data.toString());
 // });
 
-// fs.writeFileSync(filePath, "hello world");
+// const data = fs.readFileSync(filePath);
+// console.log("data", data.toString());
+
+// Delete File
+// fs.unlink(filePath, (err) => {
+//   if (err) {
+//     return console.log("Error: ", err);
+//   }
+//   console.log("File deleted!");
+// });
+
+// fs.unlinkSync(filePath);
+
+// fs.rename(filePath, filePathNew, (err) => {
+//   if (err) {
+//     return console.log("Error: ", error);
+//   }
+//   console.log("File renamed!");
+// });
+// fs.renameSync(filePath, filePathNew);
