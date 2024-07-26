2572e13ca36885b36278b5594daf057af8bedbc2
Manikant Jha <manikantcodes@gmail.com>
2024-07-26 18:34:46 +0530

    taught sending json data

--- 09-http-module.js
+++ 09-http-module.js
@@ -1,32 +1,69 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 const path = require("path");
 
+// const server = http.createServer(function (req, res) {
+//   if (req.url === "/") {
+//     res.writeHead(200, {
+//       "Content-Type": "text/html",
+//     });
+//     const data = fs.readFileSync(path.join(__dirname, "pages", "index.html"));
+//     res.write(data);
+//     res.end();
+//   } else if (req.url === "/about") {
+//     res.writeHead(200, {
+//       "Content-Type": "text/html",
+//     });
+//     res.write("<h1>About Page</h1>");
+//     res.end();
+//   } else if (req.url.startsWith("/product")) {
+//     const parsed = url.parse(req.url, true);
+//     console.log("parsed", parsed);
+//     res.writeHead(200, {
+//       "Content-Type": "text/html",
+//     });
+//     res.write("<h1>Product Page</h1>");
+//     res.end();
+//   } else if (req.url === "/contact") {
+//     res.writeHead(200, {
+//       "Content-Type": "text/html",
+//     });
+//     res.write("<h1>Contact Page</h1>");
+//     res.end();
+//   } else {
+//     res.writeHead(200, {
+//       "Content-Type": "text/html",
+//     });
+//     res.write("<h1>Page Not Found</h1>");
+//     res.end();
+//   }
+// });
+
 const server = http.createServer(function (req, res) {
-  if (req.url === "/") {
-    res.writeHead(200, {
-      "Content-Type": "text/html",
+  if (req.url === "/users") {
+    fs.readFile(path.join(__dirname, "data", "users.json"), (err, data) => {
+      if (err) {
+        res.writeHead(500, {
+          "Content-Type": "application/json",
+        });
+        res.write(
+          JSON.stringify({ success: false, msg: "Something went wrong!" })
+        );
+        res.end();
+      } else {
+        res.writeHead(200, {
+          "Content-Type": "application/json",
+        });
+        res.write(data);
+        res.end();
+      }
     });
-    const data = fs.readFileSync(path.join(__dirname, "pages", "index.html"));
-    res.write(data);
-    res.end();
-  } else if (req.url === "/about") {
-    res.writeHead(200, {
-      "Content-Type": "text/html",
-    });
-    res.write("<h1>About Page</h1>");
-    res.end();
-  } else if (req.url === "/contact") {
-    res.writeHead(200, {
-      "Content-Type": "text/html",
-    });
-    res.write("<h1>Contact Page</h1>");
-    res.end();
   } else {
-    res.writeHead(200, {
-      "Content-Type": "text/html",
+    res.writeHead(404, {
+      "Content-Type": "application/json",
     });
-    res.write("<h1>Page Not Found</h1>");
+    res.write(JSON.stringify({ success: false, msg: "Not found!" }));
     res.end();
   }
 });
