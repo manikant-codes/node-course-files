99c93cf5ce48eb04a5f21490b51c8c65fa54c3af
Manikant Jha <manikantcodes@gmail.com>
2024-07-29 12:21:36 +0530

    taught url module

--- 7-fetch.js
+++ 7-fetch.js
@@ -16,30 +16,28 @@ fetch("https://jsonplaceholder.typicode.com/users")
     //   console.log("Write completed.");
     // });
     // To CSV File
-    let text = "";
-    text += Object.keys(data[0]).toString() + "\n";
-
-    for (const value of data) {
-      let temp = "";
-      for (const key in value) {
-        if (typeof value[key] === "object") {
-          for (const k in value[key]) {
-            temp += value[key][k] + "|";
-          }
-        } else {
-          temp += value[key] + ",";
-        }
-      }
-      temp += "\n";
-      text += temp;
-    }
-
-    fs.writeFile(filePath, text, (err) => {
-      if (err) {
-        return console.log("Error: ", err);
-      }
-      console.log("Write completed.");
-    });
+    // let text = "";
+    // text += Object.keys(data[0]).toString() + "\n";
+    // for (const value of data) {
+    //   let temp = "";
+    //   for (const key in value) {
+    //     if (typeof value[key] === "object") {
+    //       for (const k in value[key]) {
+    //         temp += value[key][k] + "|";
+    //       }
+    //     } else {
+    //       temp += value[key] + ",";
+    //     }
+    //   }
+    //   temp += "\n";
+    //   text += temp;
+    // }
+    // fs.writeFile(filePath, text, (err) => {
+    //   if (err) {
+    //     return console.log("Error: ", err);
+    //   }
+    //   console.log("Write completed.");
+    // });
   })
   .catch((err) => {
     console.log("Error: ", err);
