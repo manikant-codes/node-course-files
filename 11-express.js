660ee02f64f879cc8c58342f5ce1c392562a6578
Manikant Jha <manikantcodes@gmail.com>
2024-07-31 18:42:53 +0530

    ejs

--- 11-express.js
+++ 11-express.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const path = require("path");
+const { navbar } = require("./12-components");
 
 const server = express();
 
-server.use(express.static("pages"));
+server.use(express.static("public"));
+
 server.set("view engine", "ejs");
 
 server.get("/", (req, res) => {
-  //   res.send("<h1>Hello world!</h1>");
-  //   res.status(404).send("<h1>Hello world!</h1>");
-  //   res.sendStatus(301);
-  //   res.sendFile(path.join(__dirname, "pages", "index.html"));
-  //   res.status(404).sendFile(path.join(__dirname, "pages", "index.html"));
-  //   res.download(path.join(__dirname, "pages", "01.png"));
-  //   res.json({ msg: "Hello" });
-  //   res.status(404).json({ msg: "Not Found" });
-  res.status(404).render("index", { name: "Manikant" });
+  res.render("index", { navbar });
 });
+
 server.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { navbar });
+});
+
+server.get("/contact", (req, res) => {
+  res.render("contact", { navbar });
 });
 
 server.listen(5000, () => {
