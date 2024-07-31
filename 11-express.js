87f16bb21f1648cdd61ef434fc1eb75669df7e35
Manikant Jha <manikantcodes@gmail.com>
2024-07-31 12:08:54 +0530

    ejs templating

--- 11-express.js
+++ 11-express.js
@@ -1,19 +1,17 @@
 const path = require("path");
 const express = require("express");
+const { navbar } = require("./public/components");
 const server = express();
+
+server.use("/public", express.static("public"));
 
 server.set("view engine", "ejs");
 
 server.get("/", (req, res) => {
-  //   res.sendStatus(500);
-  //   res.send("Hello world!");
-  //   res.status(404).send("Hello world!");
-  //   res.sendFile(path.join(__dirname, "pages", "index.html"));
-  //   res.status(404).sendFile(path.join(__dirname, "pages", "index.html"));
-  //   res.download(path.join(__dirname, "data", "users.json"));
-  //   res.json({ msg: "Hello world!" });
-  //   res.status(404).json({ msg: "Not found!" });
-  //   res.render("index", { name: "John Doe" });
+  res.render("index", { navbar });
+});
+server.get("/about", (req, res) => {
+  res.render("about", { navbar });
 });
 
 server.listen(5000, () => {
