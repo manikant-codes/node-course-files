89cf6a417e0eb3f5e7d027f4f0fd6fb72001abbd
Manikant Jha <manikantcodes@gmail.com>
2024-08-13 12:20:14 +0530

    update-delete controllers

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -3,7 +3,7 @@ const User = require("../models/User");
 const getAllUsers = async (req, res) => {
   try {
     const users = await User.find();
-    res.json({ data: users });
+    res.status(200).json({ data: users });
   } catch (error) {
     res.status(500).json({ msg: error.message });
   }
@@ -11,27 +11,60 @@ const getAllUsers = async (req, res) => {
 
 const getSingleUser = async (req, res) => {
   try {
-  } catch (error) {}
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ msg: "No such user found." });
+    }
+    res.status(200).json({ data: user });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 };
 
 const addUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
-    res.json({ msg: "User added!", data: user });
+    res.status(200).json({ msg: "User added.", data: user });
   } catch (error) {
-    console.log("Error: ", error);
     res.status(500).json({ msg: error.message });
   }
 };
 
 const updateUser = async (req, res) => {
   try {
-  } catch (error) {}
+    const { id } = req.params;
+    const { body } = req;
+
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ msg: "No such user found." });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(id, body, { new: true });
+
+    res.status(200).json({ msg: "User updated.", data: updatedUser });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 };
 
 const deleteUser = async (req, res) => {
   try {
-  } catch (error) {}
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ msg: "No such user found." });
+    }
+
+    await User.findByIdAndDelete(id);
+
+    res.status(200).json({ msg: "User deleted." });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const userRouter = require("./routes/usersRoutes");
-
 const { start } = require("./helpers/serverHelper");
+
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 
 server.use("/users", userRouter);
