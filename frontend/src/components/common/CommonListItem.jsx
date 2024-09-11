8d86bb5b31960f4766f67712d9ce11f9b8182a7e
Manikant Jha <manikantcodes@gmail.com>
2024-09-11 12:11:15 +0530

    removed fields sample obj from common list item

--- frontend/src/components/common/CommonListItem.jsx
+++ frontend/src/components/common/CommonListItem.jsx
@@ -3,11 +3,6 @@ import { Avatar, IconButton, Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 
-const f = {
-  image: "categoryImage",
-  title: "categoryName",
-};
-
 function CommonListItem({ item, handleDelete, link, fields }) {
   return (
     <div>
