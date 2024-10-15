cc2ab2fadbb91c750631fb79a755c7551eaf1a2d
Manikant Jha <manikantcodes@gmail.com>
2024-10-15 15:55:24 +0530

    fs module intro

---
+++ 01-fs-module.js
@@ -0,0 +1,10 @@
+// import fs from "fs";
+// const fs = require("fs");
+
+// const data = fs.readFileSync("./notes/encoding.png");
+// console.log("data", data);
+
+// fs.writeFileSync("./text-new.txt", "New Content");
+
+// const data = fs.readFileSync("./notes/encoding.png");
+// fs.writeFileSync("./encoding-backup.png", data);
