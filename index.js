478205cddca31d1d5882e278837449bee5c6f0f6
Manikant Jha <manikantcodes@gmail.com>
2024-10-15 11:09:21 +0530

    fs module

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+// import fs from "fs";
+// const fs = require("fs");
+
+// const data = fs.readFileSync("./notes/encoding.png");
+// console.log("data", data);
+
+// fs.writeFileSync("./text-new.txt", "New Content");
+
+// const data = fs.readFileSync("./notes/encoding.png");
+// fs.writeFileSync("./encoding-backup.png", data);
