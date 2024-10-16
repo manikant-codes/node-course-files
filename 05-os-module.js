fb081aec5c7b24eec700cf36a9d6665a876b2b96
Manikant Jha <manikantcodes@gmail.com>
2024-10-16 17:05:10 +0530

    os module

---
+++ 05-os-module.js
@@ -0,0 +1,15 @@
+const os = require("os");
+
+// console.log(os.arch());
+// console.log(os.constants);
+// console.log(os.cpus());
+// console.log(os.endianness());
+// console.log(os.EOL);
+// console.log(os.freemem() / 1024 / 1024 / 1024);
+// console.log(os.totalmem() / 1024 / 1024 / 1024);
+// console.log(os.hostname());
+// console.log(os.loadavg());
+// console.log(os.networkInterfaces());
+// console.log(os.type());
+// console.log(os.uptime() / 3600);
+// console.log(os.userInfo());
