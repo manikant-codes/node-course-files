1521986b074b8b867a25c7f62c93120c97d07f04
Manikant Jha <manikantcodes@gmail.com>
2024-10-18 16:44:20 +0530

    http module

---
+++ 07-http-module.js
@@ -0,0 +1,39 @@
+const http = require("http");
+const fs = require("fs");
+
+// const homePageContent = fs.readFileSync("./pages/index.html");
+
+const server = http.createServer(function (req, res) {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const readStream = fs.createReadStream("./pages/index.html", {
+      highWaterMark: 1024,
+      encoding: "utf-8"
+    });
+
+    let data = "";
+
+    readStream.on("data", function (chunk) {
+      data += chunk;
+    });
+
+    readStream.on("end", function () {
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>About Page</h1>");
+    res.end();
+  } else if (req.url === "/contact") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>Contact Page</h1>");
+    res.end();
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write("<h1>Page Not Found</h1>");
+    res.end();
+  }
+});
+
+server.listen(5000);
