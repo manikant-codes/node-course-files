de788c77d8a1ecce955049dccb6688fff27a7834
Manikant Jha <manikantcodes@gmail.com>
2024-10-22 11:06:59 +0530

    fs module

--- 03-fs-module.js
+++ 03-fs-module.js
@@ -1,10 +1,10 @@
-// import fs from "fs";
-// const fs = require("fs");
+const fs = require("fs");
 
-// const data = fs.readFileSync("./notes/encoding.png");
-// console.log("data", data);
+// const data = fs.readFileSync("./files/users/users.txt", { encoding: "utf-8" });
 
-// fs.writeFileSync("./text-new.txt", "New Content");
-
-// const data = fs.readFileSync("./notes/encoding.png");
-// fs.writeFileSync("./encoding-backup.png", data);
+// fs.readFile("./files/users/user.txt", { encoding: "utf-8" }, (error, data) => {
+//   if (error) {
+//     return console.log("Error: ", error.message);
+//   }
+//   console.log("data", data);
+// });
