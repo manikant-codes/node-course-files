c2262d2000751a0a186fb655e890c5628c886b84
Manikant Jha <manikantcodes@gmail.com>
2024-12-02 11:08:30 +0530

    routing

--- 04-http-module.js
+++ 04-http-module.js
@@ -1,24 +1,43 @@
 // Server ek computer/machine hai jise network pe aaye hue requests ke badle response bhejne keliye progream kiya hota hai.
 
-const fs = require("fs");
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    // const html = fs.readFileSync("./pages/index.html", { encoding: "utf8" });
+    // try {
     // res.writeHead(200, { "Content-Type": "text/html" });
+    // const html = fs.readFileSync("./pages/index.html", {encoding: "utf8"});
     // res.end(html);
-    // OR
+    // } catch (error) {
+    // res.writeHead(500, { "Content-Type": "text/html" });
+    // res.end("<h1>Something went wrong!</h1>");
+    // }
+
     fs.readFile("./pages/index.html", { encoding: "utf8" }, (err, data) => {
       if (err) {
         res.writeHead(500, { "Content-Type": "text/html" });
-        res.end("<h1>Something went wrong!");
+        res.end("<h1>Something went wrong!</h1>");
       } else {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end(data);
-        // OR
-        // res.write("<h1>Hello World</h1>");
-        // res.end();
+      }
+    });
+  } else if (req.url === "/index.css") {
+    fs.readFile("./pages/index.css", { encoding: "utf8" }, (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end("<h1>Something went wrong!</h1>");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/css" });
+        res.end(data);
+      }
+    });
+  } else if (req.url === "/mountains.jpg") {
+    fs.readFile("./pages/mountains.jpg", (err, data) => {
+      if (!err) {
+        res.writeHead(200, { "Content-Type": "image/jpeg" });
+        res.end(data);
       }
     });
   } else if (req.url === "/about") {
@@ -28,7 +47,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end("<h1>Contact Page</h1>");
   } else {
-    res.writeHead(404, { "Content-Type": "text/html" });
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.end("<h1>Page Not Found</h1>");
   }
 });
