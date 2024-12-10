938bc211f213b02ce7d4f319fdb6af1a8afd672e
Manikant Jha <manikantcodes@gmail.com>
2024-12-10 16:45:50 +0530

    made update and delete controllers

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -28,7 +28,6 @@ const getSingleProduct = async (req, res) => {
 
 const addProduct = async (req, res) => {
   try {
-    console.log("req.body", req.body);
     const product = await Product.create(req.body);
     res.status(200).json({ success: true, data: product });
   } catch (error) {
@@ -37,11 +36,60 @@ const addProduct = async (req, res) => {
 };
 
 const updateProduct = async (req, res) => {
-  res.send("Update Product");
+  try {
+    const { id } = req.params;
+
+    let product = await Product.findById(id);
+
+    if (!product) {
+      return res
+        .status(404)
+        .json({ success: false, msg: "No such product found." });
+    }
+
+    if (req.body.name) {
+      product.name = req.body.name;
+    }
+    if (req.body.desc) {
+      product.desc = req.body.desc;
+    }
+    if (req.body.price) {
+      product.price = req.body.price;
+    }
+    if (req.body.discountPercentage) {
+      product.discountPercentage = req.body.discountPercentage;
+    }
+
+    const updatedProduct = await product.save();
+
+    // Or, but this approach doesn't check schema.
+    // const updatedProduct = await Product.findByIdAndUpdate(id, req.body, {
+    //   new: true
+    // });
+
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, msg: error.message });
+  }
 };
 
 const deleteProduct = async (req, res) => {
-  res.send("Delete Product");
+  try {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+
+    if (!deletedProduct) {
+      return res
+        .status(404)
+        .json({ success: false, msg: "No such product found." });
+    }
+
+    res
+      .status(200)
+      .json({ success: true, msg: "Product deleted successfuly." });
+  } catch (error) {
+    res.status(500).json({ success: false, msg: error.message });
+  }
 };
 
 module.exports = {
